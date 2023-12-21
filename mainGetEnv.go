ff2f35848c4cbf3c7a0c8284af10b4ab4b426f7f
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2023-12-21 17:00:39 -0300

    Add files mainGetEnv and mainGetEnv_test

---
+++ mainGetEnv.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func GetEnv[T comparable](key string, defaultValue T) T {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	defaultValueType := reflect.TypeOf(defaultValue)
+
+	switch defaultValueType.Kind() {
+	case reflect.String:
+		return interface{}(value).(T)
+	case reflect.Slice:
+		valueSlice := strings.Split(value, ",")
+		sliceType := defaultValueType.Elem()
+		slice := reflect.MakeSlice(reflect.SliceOf(sliceType), len(valueSlice), len(valueSlice))
+		for i, v := range valueSlice {
+			sliceValue := convert(v, sliceType)
+			slice.Index(i).Set(reflect.ValueOf(sliceValue))
+		}
+		return slice.Interface().(T)
+	default:
+		return convert(value, defaultValueType).Interface().(T)
+	}
+}
+
+func convert(value string, targetType reflect.Type) reflect.Value {
+	switch targetType.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		if parsedValue, err := strconv.ParseInt(value, 10, 64); err == nil {
+			return reflect.ValueOf(parsedValue).Convert(targetType)
+		}
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		if parsedValue, err := strconv.ParseUint(value, 10, 64); err == nil {
+			return reflect.ValueOf(parsedValue).Convert(targetType)
+		}
+
+	case reflect.Bool:
+		if parsedValue, err := strconv.ParseBool(value); err == nil {
+			return reflect.ValueOf(parsedValue).Convert(targetType)
+		}
+
+	case reflect.String:
+		return reflect.ValueOf(value).Convert(targetType)
+
+	default:
+		return reflect.Zero(targetType)
+	}
+
+	return reflect.Zero(targetType)
+}
+
+func main() {
+	envVar := GetEnv("any string", "")
+	fmt.Println("The value of envVar:", envVar)
+}
---
+++ mainGetEnv_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestGetEnv(t *testing.T) {
+	t.Run("should return int8", func(t *testing.T) {
+		if val := GetEnv("TI", int8(123)); val != 123 {
+			t.Errorf("default should be int8 %s", reflect.TypeOf(val).Kind().String())
+		}
+	})
+
+	t.Run("should return int16", func(t *testing.T) {
+		if GetEnv("TI", int16(123)) != 123 {
+			t.Error("default should be int16 123")
+		}
+	})
+
+	t.Run("should return int64", func(t *testing.T) {
+		if GetEnv("TI", int16(123)) != 123 {
+			t.Error("default should be int64 123")
+		}
+	})
+
+	t.Run("should return string", func(t *testing.T) {
+		os.Setenv("TS", "test string")
+
+		if GetEnv("TS", "default string") != "test string" {
+			t.Error("Should return string 'test string")
+		}
+	})
+
+	t.Run("Should return default string", func(t *testing.T) {
+		if val := GetEnv("TS", "default string"); val != "default string" {
+			t.Errorf("Should return string 'default string' got %v", val)
+		}
+	})
+
+	t.Run("should return default fallback", func(t *testing.T) {
+		if GetEnv("TI", 123) != 123 {
+			t.Error("default should be int 123")
+		}
+	})
+
+	t.Run("should get as int", func(t *testing.T) {
+		os.Setenv("TI", "123456789")
+
+		if GetEnv("TI", 0) != 123456789 {
+			t.Error("should be int 123456789")
+		}
+	})
+
+	t.Run("should return bool", func(t *testing.T) {
+		if GetEnv("TI", true) != true {
+			t.Error("default should be bool true")
+		}
+	})
+
+	t.Run("should return bool", func(t *testing.T) {
+		os.Setenv("TI", "false")
+		if GetEnv("TI", false) != false {
+			t.Error("Should be bool false")
+		}
+	})
+
+}
